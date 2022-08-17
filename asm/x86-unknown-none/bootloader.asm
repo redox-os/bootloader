a9df22c3ec867836c9b4fc6fd0380ca66674d76f
Jeremy Soller <jackpot51@gmail.com>
2022-08-17 08:59:31 -0600

    Add space for GPT tables to BIOS bootloader

--- asm/x86-unknown-none/bootloader.asm
+++ asm/x86-unknown-none/bootloader.asm
@@ -1,16 +1,21 @@
 sectalign off
 
+; stage 1 is sector 0, loaded at 0x7C00
 %include "stage1.asm"
 
+; GPT area from sector 1 to 33, loaded at 0x7E00
+times (33*512) db 0
+
+; stage 2, loaded at 0xC000
 stage2:
     %include "stage2.asm"
     align 512, db 0
 stage2.end:
 
-; the maximum size of stage1 + stage2 is 5 KiB
-; this places stage3 at 0x9000
-times 5120-($-$$) db 0
+; the maximum size of stage2 is 4 KiB
+times (4*1024)-($-stage2) db 0
 
+; stage3, loaded at 0xD000
 stage3:
     %defstr STAGE3_STR %[STAGE3]
     incbin STAGE3_STR
