25b1813ad9c56ba025db7e27a3e12fa2d5953fc2
Jeremy Soller <jackpot51@gmail.com>
2024-10-25 20:19:05 -0600

    Fix parsing of raspi 400 DTB

--- src/os/uefi/dtb.rs
+++ src/os/uefi/dtb.rs
@@ -29,49 +29,46 @@ pub unsafe fn is_in_dev_mem_region(addr: usize) -> bool {
 }
 
 unsafe fn get_dev_mem_region(fdt: &Fdt) {
-    let soc = fdt.find_node("/soc");
-    let cell_sizes = fdt.root().cell_sizes();
+    let Some(soc) = fdt.find_node("/soc") else { return };
+    let Some(ranges) = soc.property("ranges") else { return };
+    let cell_sizes = soc.cell_sizes();
     let chunk_size = (cell_sizes.address_cells * 2 + cell_sizes.size_cells) * 4;
-    if let Some(soc) = soc {
-        if let Some(ranges) = soc.property("ranges") {
-            for chunk in ranges.value.chunks(chunk_size) {
-                let child_bus_addr = {
-                    if cell_sizes.address_cells == 1 {
-                        BE::read_u32(&chunk[0..4]) as u64
-                    } else if cell_sizes.address_cells == 2 {
-                        BE::read_u32(&chunk[0..8]) as u64
-                    } else {
-                        DEV_MEM_AREA.clear();
-                        return;
-                    }
-                };
-                let parent_bus_addr = {
-                    if cell_sizes.address_cells == 1 {
-                        BE::read_u32(&chunk[4..8]) as u64
-                    } else if cell_sizes.address_cells == 2 {
-                        BE::read_u32(&chunk[8..16]) as u64
-                    } else {
-                        DEV_MEM_AREA.clear();
-                        return;
-                    }
-                };
-                let addr_size = {
-                    if cell_sizes.address_cells == 1 {
-                        BE::read_u32(&chunk[8..12]) as u64
-                    } else if cell_sizes.address_cells == 2 {
-                        BE::read_u32(&chunk[16..24]) as u64
-                    } else {
-                        DEV_MEM_AREA.clear();
-                        return;
-                    }
-                };
-                println!(
-                    "dev mem 0x{:08x} 0x{:08x} 0x{:08x}",
-                    child_bus_addr, parent_bus_addr, addr_size
-                );
-                DEV_MEM_AREA.push((parent_bus_addr as usize, addr_size as usize));
+    for chunk in ranges.value.chunks(chunk_size) {
+        let child_bus_addr = {
+            if cell_sizes.address_cells == 1 {
+                BE::read_u32(&chunk[0..4]) as u64
+            } else if cell_sizes.address_cells == 2 {
+                BE::read_u32(&chunk[0..8]) as u64
+            } else {
+                DEV_MEM_AREA.clear();
+                return;
             }
-        }
+        };
+        let parent_bus_addr = {
+            if cell_sizes.address_cells == 1 {
+                BE::read_u32(&chunk[4..8]) as u64
+            } else if cell_sizes.address_cells == 2 {
+                BE::read_u32(&chunk[8..16]) as u64
+            } else {
+                DEV_MEM_AREA.clear();
+                return;
+            }
+        };
+        let addr_size = {
+            if cell_sizes.size_cells == 1 {
+                BE::read_u32(&chunk[8..12]) as u64
+            } else if cell_sizes.size_cells == 2 {
+                BE::read_u32(&chunk[16..20]) as u64
+            } else {
+                DEV_MEM_AREA.clear();
+                return;
+            }
+        };
+        println!(
+            "dev mem 0x{:08x} 0x{:08x} 0x{:08x}",
+            child_bus_addr, parent_bus_addr, addr_size
+        );
+        DEV_MEM_AREA.push((parent_bus_addr as usize, addr_size as usize));
     }
 }
 
